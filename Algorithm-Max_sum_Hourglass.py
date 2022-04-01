6d83c88eb6b579bac8c208172343260a248fe02c
Hariprasath G <hariprasath167@gmail.com>
2022-04-01 18:19:13 +0530

    Algorithm-Max_sum_Hourglass

--- Algorithm-Max_sum_Hourglass.py
+++ Algorithm-Max_sum_Hourglass.py
@@ -1,5 +1,5 @@
 def hourglass(arr):
-    # Iniatialze the count i.e., result value as 0
+    # Iniatialze the result value as 0
     result = 0
 
     # We are considering only inner matrix for time efficiency i.e, from arr[1][1] till arr[len(arr[0] - 1)][len(arr[0] - 1)]
@@ -10,20 +10,21 @@ def hourglass(arr):
           d
         e f g 
     """
-    # val = arr[i][j] , we have to check all the surrounding values i.e., a,b,c,e,f and j are == val or not..
-    # If so increment the result value by 1
+    # val = arr[i][j] , we have to add all the surrounding values i.e., a,b,c,d,e,f and j..
+    # Update the result value with val only if the current val is greater than previous result
     for i in range(1, len(arr) - 1):
         for j in range(1, len(arr[0])-1):
-            val = arr[i][j]
             a = arr[i - 1][j - 1]
             b = arr[i - 1][j]
             c = arr[i - 1][j + 1]
+            d = arr[i][j]
             e = arr[i + 1][j - 1]
             f = arr[i + 1][j]
             g = arr[i + 1][j + 1]
-
-            if (a == val) and (b == val) and (c == val) and (e == val) and (f == val) and (g == val):
-                result += 1
+            
+            val = a + b + c + d + e + f + g
+            if val > result:
+                result = val
 
     return result
 if __name__ == "__main__":
